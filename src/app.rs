56fc081b8fa66d4c4a5b98a21dfc55da6e2cd63a
Paweł Zatoka <github@hoxi.me>
2026-07-26 05:49:42 +0200

    feat: adjust save prompt buttons
    swaps "Save" and "Cancel" since having "Cancel" be first feels very
    counterintuitive?

--- src/app.rs
+++ src/app.rs
@@ -831,7 +831,7 @@ impl AppModel {
             .detail(format!("There are unsaved changes to {what}."))
             .modal(true)
             .build();
-        dialog.set_buttons(&["Cancel", "Discard Changes", "Save"]);
+        dialog.set_buttons(&["Save", "Discard Changes", "Cancel"]);
         dialog.set_cancel_button(0);
         dialog.set_default_button(2);
         let sender = sender.clone();
@@ -840,7 +840,7 @@ impl AppModel {
             gio::Cancellable::NONE,
             move |response| match response.ok() {
                 Some(1) => sender.input(AppMsg::ExecuteAction(action)),
-                Some(2) => sender.input(AppMsg::SaveBeforeAction(action)),
+                Some(0) => sender.input(AppMsg::SaveBeforeAction(action)),
                 _ => {}
             },
         );
